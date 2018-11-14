f9a725635dd9a95d690d1fc7e1dd12a92f19e946
devtdeng <tdeng@pivotal.io>
2018-11-14 16:50:04 +0100

    add head test code

--- producer-demo/src/main/java/com/example/producerdemo/ProducerDemoApplication.java
+++ producer-demo/src/main/java/com/example/producerdemo/ProducerDemoApplication.java
@@ -3,6 +3,9 @@ package com.example.producerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,5 +25,17 @@ public class ProducerDemoApplication {
         String cfInstnaceIndex = System.getenv("CF_INSTANCE_INDEX");
     
         return "Response from:" + "<br />" + "CF_INSTANCE_ADDR: " + cfInstanceAddr + "<br />" + "CF_INSTANCE_INTERNAL_IP: " + cfInstanceInternalIP + "<br />" + "CF_INSTANCE_INDEX " + cfInstnaceIndex;
+    }
+
+    @RequestMapping("/test_header")
+    public ResponseEntity<String> testHeader() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Cache-Control", "public");
+        responseHeaders.add("sync-control", "max-age=1");
+        responseHeaders.add("ALLCAPS", "ALLCAPS");
+        responseHeaders.add("alllower", "alllower");
+        responseHeaders.add("CamelCase", "CamelCase");
+        responseHeaders.add("UpDoWn", "UpDoWn");
+        return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
     }
 }
