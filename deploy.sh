1c87ebc147db03b7c98a7992c66e85365c00252a
devtdeng <tdeng@pivotal.io>
2018-11-22 11:21:00 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ cf cs p-service-registry standard service-registry
 cf cs -c '{"git":{"uri":"https://github.com/devtdeng/config-server-configurations"},"count":1}' p-config-server standard config-server
 cf cs  p-circuit-breaker-dashboard standard cbd
 
+sleep 60
+
 pushd consumer-demo
     mvn package -Dmaven.test.skip
     cf push
